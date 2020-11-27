1e2a9ee5ae85ebe7a6390159e36fbcda0ad8a97f
Ricardo Dias <cadu.hd@gmail.com>
2020-11-27 17:50:44 -0300

    add list with style

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => 
+    setEnteredGoal(enteredText);
+  
+  const addGoalHandler = () => 
+    setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: enteredGoal}])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={styles.container}>
+        <TextInput 
+        placeholder="Course Goal" 
+        style={styles.input}
+        onChangeText={goalInputHandler}
+        value={enteredGoal}/>
+
+        <Button title="Add" onPress={addGoalHandler}></Button>
+      </View>
+      <FlatList data={courseGoals} renderItem={itemData => (
+        <View style={styles.listItem}>
+          <Text>
+            {itemData.item.value}
+          </Text>
+        </View>
+      )}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 40,
+    alignItems: 'center'
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#000',
+    width: '80%',
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
